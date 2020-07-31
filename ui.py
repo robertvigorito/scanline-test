c6cd19d790069625365c01d793d7109fc644d417
Robert Vigorito <robertvigorito@gmail.com>
2020-07-31 16:44:24 -0400

    WIP: updating progress

--- ui.py
+++ ui.py
@@ -13,9 +13,11 @@ should attempt to set that knob on all nodes of the matching class to the users 
 """
 import sys
 import nuke
+import re
 
 try:
     from PySide import QtGui, QtCore
+    QtWidgets = QtGui
 except:
     from PySide2 import QtGui, QtCore, QtWidgets
 
@@ -35,24 +37,101 @@ def all_class_knobs(cls):
     return []
 
 
+def is_int_or_str(attr):
+    """
+    Custom type check to see if the attribute is a string or int.
+
+    Args:
+        attr(str):      String type
+
+    Returns:
+        (int|str)
+    """
+    if attr.isdigit():
+        return int(attr)
+
+    return str(attr)
+
+def filter_format(attr):
+    """
+    Find the format by int list
+    Args:
+        attr:
+
+    Returns:
+
+    """
+    if not attr.isdigit():
+        raise TypeError("Please provide an int value!")
+
+    if int(attr) == 0:
+        return nuke.root().format().name()
+
+    nuke_format = nuke.formats()[int(attr)-1]
+    return nuke_format.name()
+
+
 class CustomLineEdit(QtWidgets.QLineEdit):
+    """
+    Custom line edit, that populates the completer on click. The
+    populate functions required to return a list of string to fill
+    in the completer.
+
+    Attributes:
+        items(list):        List of completer items
+    """
     def __init__(self, populate):
+        """
+        Init constructor.
+
+        Args:
+            populate(function):         Callable function that returns a list
+        """
         super(CustomLineEdit, self).__init__()
         self.populate = populate
         self.items = list()
+        # Populate if you can on init
+        self.mousePressEvent()
 
-    def mousePressEvent(self, event):
+    def mousePressEvent(self, *args, **kwargs):
+        """
+        On mouse click, if there is a value highlight the text input, update the completer
+        from the populate function.
+        """
+        # Select the input, making it easier to remove/override the input text
+        self.selectAll()
+        # Set the list of string from the populate function, list of classes or knobs
         self.items = self.populate()
+        # Create and set the completer
         completer = QtWidgets.QCompleter(self.items)
         completer.setCaseSensitivity(QtCore.Qt.CaseInsensitive)
         self.setCompleter(completer)
-
         self.completer().setCompletionPrefix(self.text() or "")
+        # Pop up the completer
         self.completer().complete()
+
+        return None
 
 
 class GatherAndSet(QtWidgets.QDialog):
+    __KNOB_TYPES = \
+        {
+            "Boolean_Knob": int,
+            "ChannelMask_Knob": str,
+            "Enumeration_Knob": is_int_or_str,
+            "Int_Knob": int,
+            "Format_Knob": filter_format,
+        }
+    __PATTERN = re.compile(r"\w+")
+
     def __init__(self, parent=None):
+        """
+        Set the layout and connection on launch.
+
+        Args:
+            parent(object):        Parent QWidget, or app application
+        """
+
         super(GatherAndSet, self).__init__(parent or QtWidgets.QApplication.activeWindow())
         self.resize(350, 150)
 
@@ -61,40 +140,37 @@ class GatherAndSet(QtWidgets.QDialog):
 
     @property
     def classes(self):
+        """ (list) of class names. """
         return self.class_dropdown.items
 
     @property
     def class_name(self):
+        """ (str) Current class name. """
         return self.class_dropdown.text()
 
     @property
     def knob_name(self):
+        """ (list) of knob names. """
         return self.knob_dropdown.text()
 
     @property
     def knobs(self):
+        """ (str) Current knob name. """
         return self.knob_dropdown.items
 
     @property
     def value(self):
+        """ (str) User input value. """
         return self.value_lineedit.text()
 
-    def connection(self):
-        self.close_button.pressed.connect(self.close)
-        self.set_button.pressed.connect(self.set_values)
-
-    def set_values(self):
-        if not all([self.class_name in self.classes, self.knob_name in self.knobs]):
-            # do message
-            return False
-
-        for node in nuke.allNodes(self.class_name):
-            node.knob(self.knob_name).setValue(self.value)
-
-
-
-
     def base_layout(self):
+        """
+        Creating the class, knob, and input value line edits, adding the
+        widgets to the grid layout.
+
+        Returns:
+            (QtWidgets.QGridLayout)
+        """
         self.class_dropdown = CustomLineEdit(all_classes)
         self.class_dropdown.setPlaceholderText("Selected Node Class!")
 
@@ -115,6 +191,13 @@ class GatherAndSet(QtWidgets.QDialog):
         return layout
 
     def button_layout(self):
+        """
+        Creating the set, and closepush buttons, appending the button to the
+        layout.
+
+        Returns:
+            (QtWidgets.QHBoxLayout)
+        """
         self.set_button = QtWidgets.QPushButton("Set")
         self.close_button = QtWidgets.QPushButton("Close")
 
@@ -124,7 +207,22 @@ class GatherAndSet(QtWidgets.QDialog):
 
         return layout
 
+    def connection(self):
+        """
+        Handling the signal connections for the ui.
+        """
+        self.close_button.pressed.connect(self.close)
+        self.set_button.pressed.connect(self.set_values)
+
+        return None
+
     def master_layout(self):
+        """
+        Combining all the layouts into the master.
+
+        Returns:
+            (QtWidgets.QVBoxLayout)
+        """
         layout = QtWidgets.QVBoxLayout()
 
         layout.addLayout(self.base_layout())
@@ -133,6 +231,42 @@ class GatherAndSet(QtWidgets.QDialog):
 
         return layout
 
+    def set_values(self):
+        if not all([self.class_name in self.classes, self.knob_name in self.knobs]):
+            msg = "Please select valid class and knob!"
+            QtWidgets.QMessageBox(self, text=msg, icon=QtWidgets.QMessageBox.Warning).exec_()
+            return False
+
+        if not self.value:
+            msg = "Value required for the set operation, read tooltip for more information!"
+            QtWidgets.QMessageBox(self, text=msg, icon=QtWidgets.QMessageBox.Warning).exec_()
+
+        try:
+            for node in nuke.allNodes(self.class_name):
+                knob = node.knob(self.knob_name)
+                knob_type = self.__KNOB_TYPES.get(knob.Class(), str)
+                # Check the about of array
+                print knob
+                if hasattr(knob, "arraySize"):
+                    array = knob.arraySize()
+                    value = self.__PATTERN.findall(self.value)
+
+                    if array == 1:
+                        value = knob_type(self.value)
+                    elif len(value) != array and len(value) == 1:
+                        value = [self.value] * array
+                    elif array != len(value):
+                        raise TypeError("Array doesnt match! Expected array length of {}, {} provided!".format(
+                            array, len(value)))
+                else:
+                    value = knob_type(self.value)
+
+                node.knob(self.knob_name).setValue(value)
+
+        except Exception as e:
+            import logging
+            logging.exception("")
+            QtWidgets.QMessageBox(self, text=e.message.title(), icon=QtWidgets.QMessageBox.Information).exec_()
 
 
 def run_in_nuke():
@@ -141,9 +275,3 @@ def run_in_nuke():
 
     return True
 
-
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    g = GatherAndSet()
-    g.show()
-    sys.exit(app.exec_())
